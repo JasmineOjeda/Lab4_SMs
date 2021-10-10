38b9add3366651c1e32d91012982e0accccad6a9
Ubuntu <jojed016@ucr.edu>
2021-10-09 23:53:42 -0700

    Starting part 1

--- source/main.c
+++ source/main.c
@@ -1,23 +1,83 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*      Author: Jasmine Ojeda jojed016@ucr.edu
+ *      Lab Section: 022
+ *      Assignment: Lab 4  Exercise 1
+ *      Exercise Description: LEDS and buttons
  *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum States {START, OFF_RELEASE, OFF_PRESS, ON_RELEASE, ON_PRESS} state;
+
+void Tick() {
+    /* State transitions */
+    switch(state) {
+        case START:
+            state = OFF_RELEASE;
+            PORTB = 0x01;
+            break;
+        case OFF_RELEASE:
+            if (PINA == 0x01) {
+                state = ON_PRESS;
+            }
+            else {
+                state = OFF_RELEASE;
+            }
+            break;
+        case ON_PRESS:
+            if (PINA == 0x01) {
+                state = ON_PRESS;
+            }
+            else {
+                state = ON_RELEASE;
+            }
+            break;
+        case ON_RELEASE:
+            if (PINA == 0x01) {
+                state = OFF_PRESS;
+            }
+            else {
+                state = ON_RELEASE;
+            }
+            break;
+        case OFF_PRESS:
+            if (PINA == 0x01) {
+                state = OFF_PRESS;
+            }
+            else {
+                state = OFF_RELEASE;
+            }
+            break;
+        default:
+	    break; 
+    }
+
+    /* State actions */
+    switch(state) {
+        case OFF_RELEASE:
+            PORTB = 0x01;
+            break;
+        case ON_RELEASE:
+            PORTB = 0x02;
+            break;
+        default:
+            break;
+    }
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
 
+    PORTB = 0x01;
     /* Insert your solution below */
     while (1) {
-
+        Tick();
     }
     return 1;
 }
